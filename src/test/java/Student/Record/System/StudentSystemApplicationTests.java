04cc81a74b10ccb7d94406284903bc77a456a42f
Andrei Boacă <adi.boaca@gmail.com>
2026-04-28 13:12:47 +0200

    Initial commit from Spring Initializr

---
+++ src/test/java/Student/Record/System/StudentSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package Student.Record.System;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
